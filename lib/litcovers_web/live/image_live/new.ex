744d46f573d1b10bd4f84b088aa0ce760b84454a
Kitasio <kit.plotits@gmail.com>
2023-04-02 15:08:40 +0200

    blocked image removed for now

--- lib/litcovers_web/live/image_live/new.ex
+++ lib/litcovers_web/live/image_live/new.ex
@@ -544,9 +544,9 @@ defmodule LitcoversWeb.ImageLive.New do
         >
           1:1
         </div>
-        <div class="w-32 bg-zinc-600 opacity-50 flex items-center justify-center gap-1 px-4 py-2.5 cursor-pointer rounded-xl border-2 border-stroke-main bg-tag-main">
-          <.icon name="hero-lock-closed" class="w-5 h-5" /> <span>3:2</span>
-        </div>
+        <!-- <div class="w-32 bg-zinc-600 opacity-50 flex items-center justify-center gap-1 px-4 py-2.5 cursor-pointer rounded-xl border-2 border-stroke-main bg-tag-main"> -->
+        <!--   <.icon name="hero-lock-closed" class="w-5 h-5" /> <span>3:2</span> -->
+        <!-- </div> -->
       </div>
     </div>
     """
