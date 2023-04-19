755568dae33d940040aaacf0d94f728f0c9414cf
Kitasio <kit.plotits@gmail.com>
2023-04-19 12:58:31 +0200

    changed negative prompt

--- lib/cover_gen/replicate/input.ex
+++ lib/cover_gen/replicate/input.ex
@@ -1,7 +1,8 @@
 defmodule CoverGen.Replicate.Input do
   @derive Jason.Encoder
   defstruct prompt: "multicolor hyperspace",
-            negative_prompt: "three heads, three faces, fingers, hands, four heads, four faces",
+            negative_prompt:
+              "ugly, tiling, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, extra limbs, disfigured, deformed, body out of frame, bad anatomy, watermark, signature, cut off, low contrast, underexposed, overexposed, bad art, beginner, amateur, distorted face, blurry, draft, grainy",
             width: 512,
             height: 768,
             num_outputs: 1,
