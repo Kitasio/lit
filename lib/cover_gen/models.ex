9884cef3c683cd899e5d2bc6485a7fc65e29c90b
Kitasio <kit.plotits@gmail.com>
2024-07-18 12:59:57 +0200

    Add prices for models

--- lib/cover_gen/models.ex
+++ lib/cover_gen/models.ex
@@ -5,4 +5,6 @@ defmodule CoverGen.Models do
   Returns the price of the model
   """
   def price(%User{} = user, "sd3"), do: 10 * user.discount
+  def price(%User{} = user, "core"), do: 8 * user.discount
+  def price(%User{} = user, "ultra"), do: 12 * user.discount
 end
